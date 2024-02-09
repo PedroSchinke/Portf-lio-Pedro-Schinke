c8e8366bf5f1320f1585c84cd8af5f366a0e8d7c
Pedro Schinke <pedro.schinke@outlook.com>
2024-02-08 21:46:26 -0300

    title and icon change

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,7 +1,4 @@
 import { Arrows, ContentContainer, Header, HeroContainer } from './styles'
-// import { Profile } from '../Profile/Profile'
-// import { Projects } from '../projects/Projects'
-// import { Contact } from '../contact/Contact'
 import { RefObject, useRef, Suspense, lazy } from 'react'
 
 const Profile = lazy(() => import('../Profile/Profile'))
@@ -31,7 +28,7 @@ export function Home() {
             </ul>
           </nav>
         </Header>
-        <h1>Procurando um desenvolvedor FullStack?</h1>
+        <h1>Procurando um desenvolvedor Full Stack?</h1>
         <Arrows>
           <path className="a1" d="M0 0 L30 32 L60 0"></path>
           <path className="a2" d="M0 20 L30 52 L60 20"></path>
